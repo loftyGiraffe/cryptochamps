e8e164bfd601b6a9effa7412af3495bc27067e40
loftyGiraffe <94640313+loftyGiraffe@users.noreply.github.com>
2021-12-16 22:29:21 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
 import { Web3ReactProvider } from "@web3-react/core";
 import Web3 from "web3";
-import "./index.css";
 
 function getLibrary(provider) {
   return new Web3(provider);
