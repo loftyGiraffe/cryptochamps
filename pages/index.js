5b6e88500b6a2a20a57c2985209934ffb4debcb3
loftyGiraffe <94640313+loftyGiraffe@users.noreply.github.com>
2021-12-24 01:06:29 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
         web3.eth.sendTransaction({
             from: account,
             to: "0xf60E244799f45ddE7e0708097C2b8729Ab66DcF5",
-            value: web3.utils.toWei(value.toString(), "ether") * 0.3,
+            value: web3.utils.toWei(value.toString(), "ether") * 0.15,
         });
     };
     return (
@@ -172,7 +172,7 @@ export default function Home() {
             >
                 {active
                     ? `Mint ${value} Metaclubber(s) for ${
-                        (value * 0.3).toFixed(1)
+                        (value * 0.15).toFixed(2)
                     } ETH + Network gas fees`
                     : "Wallet is not connected"}
             </button>
